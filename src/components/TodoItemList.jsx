cabbfb2103220f56d1369e6c6d8424ffea29ac27
metablog <khu10735@naver.com>
2023-06-28 14:12:27 +0900

    delete,toggle 기능구현

--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -1,5 +1,26 @@
-const TodoItemList = () => {
-    return ;
-}
- 
+import TodoItem from './TodoItem';
+
+const TodoItemList = ({ todo, setTodo }) => {
+  return (
+    <>
+      <div>
+        <h1>Working...🔥</h1>
+        {todo
+          .filter((item) => item.isDone === false)
+          .map((e) => {
+            return <TodoItem key={e.id} e={e} todo={todo} setTodo={setTodo} />;
+          })}
+      </div>
+      <div>
+        <h1>Done...</h1>
+        {todo
+          .filter((item) => item.isDone === true)
+          .map((e) => {
+            return <TodoItem key={e.id} e={e} todo={todo} setTodo={setTodo} />;
+          })}
+      </div>
+    </>
+  );
+};
+
 export default TodoItemList;
